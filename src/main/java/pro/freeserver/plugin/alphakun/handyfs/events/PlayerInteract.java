e9aa8802d4b07122be5ef4b400f27970bb27dea2
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2021-05-08 18:55:18 +0900

    [Fixed] menuを開いたあと操作ができないバグを解消

--- src/main/java/pro/freeserver/plugin/alphakun/handyfs/events/PlayerInteract.java
+++ src/main/java/pro/freeserver/plugin/alphakun/handyfs/events/PlayerInteract.java
@@ -1,36 +1,22 @@
 package pro.freeserver.plugin.alphakun.handyfs.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.Objects;
-
-import static pro.freeserver.plugin.alphakun.handyfs.Handyfs.*;
 
 public class PlayerInteract implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         ItemStack item = p.getInventory().getItemInMainHand();
-        delay(item,p);
-    }
-
-    private static void delay(ItemStack item, Player p) {
-        new BukkitRunnable() {
-
-            @Override
-            public void run() {
-                if (item.getType().equals(Material.BOOK) && !(p.getOpenInventory() instanceof PlayerInventory)) {
-                    p.chat("/menu");
-                }
+        if (item.getType().equals(Material.BOOK)) {
+            if (e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                p.chat("/menu");
             }
-        }.runTaskLater(plugin,2L);
+        }
     }
 }
